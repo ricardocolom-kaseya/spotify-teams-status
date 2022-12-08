94c948de46b3bd0f7bde7c8631bca1fa7bc6b4c9
Ricardo Colom <ricardo.colom@kaseya.com>
2022-12-08 10:41:47 -0500

    Added a modal that informs the user and asks them to input their Teams Token, songs are now pushed to the backend on song change or token validation

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,11 +3,22 @@ import useAuth from './useAuth'
 
 import axios from 'axios'
 import SpotifyWebApi from "spotify-web-api-node"
-import { Button, HStack, VStack, Text, Box, Image, Checkbox, IconButton, Icon, Divider, LightMode } from '@chakra-ui/react'
+import { Button, HStack, VStack, Text, Box, Image, Checkbox, IconButton, Icon, Divider, LightMode, useDisclosure, Heading, Input } from '@chakra-ui/react'
 import { FaPlay, FaPause, FaStepBackward, FaStepForward } from 'react-icons/fa'
-import { SiMicrosoftteams } from 'react-icons/si'
+import { SiMicrosoftteams, SiTampermonkey } from 'react-icons/si'
+import { MdSave } from 'react-icons/md'
 
 import useInterval from './useInterval'
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 
 const spotifyApi = new SpotifyWebApi({
   clientId: "a248cdcebd804022917a3c7fc1d66d76",
@@ -39,8 +50,12 @@ const SongCard = ({ playingTrack }) => {
   )
 }
 
-const StatusBar = ({ teamsToken, isPlaying, pauseOrResumeSong, setPushToTeams, previousSong, nextSong }) => {
+const StatusBar = ({ teamsToken, setTeamsToken, isPlaying, pauseOrResumeSong, setPushToTeams, previousSong, nextSong }) => {
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const [modalToken, setModalToken] = useState("")
+  const [doSave, setDoSave] = useState("false")
 
   const teamsButton = () => {
     if (teamsToken) {
@@ -56,14 +71,63 @@ const StatusBar = ({ teamsToken, isPlaying, pauseOrResumeSong, setPushToTeams, p
     }
     else {
       return (
-        <LightMode>
-          <Button colorScheme="purple" size="sm" variant="solid">
-            <HStack>
-              <Icon as={SiMicrosoftteams} w="20px" h="20px" color="white" />
-              <Text color="white">Log in to Microsoft Teams</Text>
-            </HStack>
-          </Button>
-        </LightMode>
+        <>
+          <LightMode>
+            <Button colorScheme="purple" size="sm" variant="solid" onClick={onOpen}>
+              <HStack>
+                <Icon as={SiMicrosoftteams} w="20px" h="20px" color="white" />
+                <Text color="white">Get your Teams status token</Text>
+              </HStack>
+            </Button>
+          </LightMode>
+          <Modal isOpen={isOpen} onClose={onClose} isCentered size="lg" onCloseComplete={() => {
+            if (doSave) {
+              setDoSave(false)
+              setTeamsToken(modalToken)
+            }
+          }}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Get your Teams status token</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <VStack spacing="6">
+                  <Heading size="md">What is this?</Heading>
+                  <Text>Every Microsoft Teams user has a unique token that is used to make changes to your current status. This token is reset about every day or so.</Text>
+                  <Text>I wrote a script to make it easy to get your token after logging into teams on your browser.</Text>
+                  <LightMode>
+                    <HStack spacing="2" w="100%" justifyContent="space-between">
+                      <Button colorScheme="blackAlpha" size="sm" variant="solid" w="216px" >
+                        <HStack w="100%">
+                          <Icon as={SiTampermonkey} w="20px" h="20px" color="white" />
+                          <Text color="white" w="100%">Get the script</Text>
+                        </HStack>
+                      </Button>
+                      <Button colorScheme="purple" size="sm" variant="solid" w="216px">
+                        <HStack w="100%">
+                          <Icon as={SiMicrosoftteams} w="20px" h="20px" color="white" />
+                          <Text color="white" w="100%">Log in to Microsoft Teams</Text>
+                        </HStack>
+                      </Button>
+                    </HStack>
+                  </LightMode>
+                  <HStack w="100%">
+                    <Input w="100%" variant="outline" placeholder='Paste your teams token here' onChange={(e) => setModalToken(e.target.value)} />
+                    <Button variant="outline" colorScheme="blue" rightIcon={<Icon as={MdSave} w={4} h={4} />} onClick={() => { setDoSave(true); onClose() }}>Save</Button>
+                  </HStack>
+                </VStack>
+              </ModalBody>
+            </ModalContent>
+            <ModalFooter>
+              <LightMode>
+                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                  Close
+                </Button>
+                <Button variant='ghost'>Secondary Action</Button>
+              </LightMode>
+            </ModalFooter>
+          </Modal>
+        </>
       )
     }
   }
@@ -82,12 +146,12 @@ const StatusBar = ({ teamsToken, isPlaying, pauseOrResumeSong, setPushToTeams, p
   )
 }
 
-const TEAMS_TOKEN = "eyJ0eXAiOiJKV1QiLCJub25jZSI6InJxNjVOcnNPZmhiOWlIZVQtdGs1WFo1c3kxN1g1Zk10RGZRRXR4Vi12a28iLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiJodHRwczovL3ByZXNlbmNlLnRlYW1zLm1pY3Jvc29mdC5jb20vIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYTFjZDM0MzYtNjA2Mi00MTY5LWExYmQtNzllZmRjZmQ4YTVlLyIsImlhdCI6MTY3MDM0NDEyNiwibmJmIjoxNjcwMzQ0MTI2LCJleHAiOjE2NzA0MTc5NTQsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUFXU3MrR3pGeFNEekJ2OTk0UFd1b3NRd3laZnB0R2l0c1VnSnNXQkRhRFVZNWJUUi80SEpIYnJVMVVQaDRhanhxY29QWGpFeGtYaGNtZDVFdlRCWURUY1JNVTl2KzlYR2lnaGZmZEVZdncvRT0iLCJhbXIiOlsicHdkIiwid2lhIiwibWZhIl0sImFwcGlkIjoiNWUzY2U2YzAtMmIxZi00Mjg1LThkNGItNzVlZTc4Nzg3MzQ2IiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJDb2xvbSIsImdpdmVuX25hbWUiOiJSaWNhcmRvIiwiaXBhZGRyIjoiMTcwLjU1LjE0NS4yNDIiLCJuYW1lIjoiUmljYXJkbyBDb2xvbSIsIm9pZCI6IjIxMGFmODZmLWFhMzctNGU0Yi04MTA5LTcxZDZkY2Q0MjdhMCIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0xMTM3MTA1OTYtMzExODI0MDU4LTczMDczNTY0MC0yOTcyNyIsInB1aWQiOiIxMDAzMjAwMjQ2RTIwOTlEIiwicmgiOiIwLkFTd0FOalROb1dKZ2FVR2h2WG52M1AyS1hpZk5jQjRIUjRsRmpzV2IwZ3hIS2tZc0FEdy4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJxcGtyUF9zNmI2MDVvOURJTm80SGhYRTJYU0M3SUktRUNqSlFrT1FmQXZ3IiwidGlkIjoiYTFjZDM0MzYtNjA2Mi00MTY5LWExYmQtNzllZmRjZmQ4YTVlIiwidW5pcXVlX25hbWUiOiJyaWNhcmRvLmNvbG9tQGthc2V5YS5jb20iLCJ1cG4iOiJyaWNhcmRvLmNvbG9tQGthc2V5YS5jb20iLCJ1dGkiOiJ5b29aekFzZXdFaTVDRk5YY19oMUFBIiwidmVyIjoiMS4wIiwieG1zX2NjIjpbIkNQMSJdLCJ4bXNfc3NtIjoiMSJ9.Rq41F2CtMwkDTl2kK-G99mH_IIRjROWPrgZEFgT0yFQm-sIyQFKDZV1QdEgABaSMICNZQym-uGVfrtWyUdvDzF0z5cv52BQ1Zl62eYVRO3zQsOhOHdzNvD680eocjpTRyxcZGpB3KHyll-fbXA6UeIrKdM_4iSamFbXL_VNS-_MFdf5NUYuvdNnBVwOVkgtvpXMTM8ZFsFE6bgTdvI-zdJqFPZyKPcSZIidkXxT9wK2zfucNMdfUFx1sVKWrtHN5SJIhxJCkA2DrxPbvcHyd71QDiWJhPY-O-QyyYmMGHjE8GpNA5kfbrN5W0IysdBKNXGnseAFWnpeDQVRG7BTURA"
-
 export default function Dashboard({ code }) {
 
   const accessToken = useAuth(code)
-  const [teamsToken, setTeamsToken] = useState(TEAMS_TOKEN)
+  const [teamsToken, setTeamsToken] = useState(undefined)
+
+  useEffect(() => { console.log("fired") }, [teamsToken])
 
   const [userName, setUserName] = useState("No name")
   const [playingTrack, setPlayingTrack] = useState(
@@ -152,7 +216,6 @@ export default function Dashboard({ code }) {
       })
   }
 
-
   function nextSong() {
     spotifyApi.skipToNext()
       .then(function () {
@@ -175,7 +238,7 @@ export default function Dashboard({ code }) {
           axios.put("http://localhost:4000/status", message, {
             headers: {
               "Content-Type": "application/json",
-              "Authorization": `Bearer ${TEAMS_TOKEN}`
+              "Authorization": `Bearer ${teamsToken}`
             }
           }).then(response => {
             //console.log(response)
@@ -227,7 +290,6 @@ export default function Dashboard({ code }) {
                   )
                 }
 
-                //console.log(artists[0].name + " - " + name)
               }
             }, function (err) {
               console.log('Something went wrong!', err);
@@ -251,24 +313,37 @@ export default function Dashboard({ code }) {
 
   useInterval(() => getCurrentSong(), 1000)
 
-  useInterval(() => forcePushToTeams(), (pushToTeams) ? 10000 : null)
-
   useEffect(() => {
     console.log("Track changed")
+
+    if (teamsToken != undefined) {
+      console.log("test")
+      forcePushToTeams()
+    }
   }, [playingTrack])
 
+
+
+  useEffect(() => {
+    if (teamsToken != undefined) {
+      console.log("test")
+      forcePushToTeams()
+    }
+  }, [teamsToken])
   const DashboardContent = () => {
     if (accessToken != undefined) {
       return (
         <VStack w="100vw" h="100vh" justify="center" align="center" spacing="6">
           <Text fontSize="2xl">Hey, {userName}</Text>
-          <VStack borderRadius="lg" spacing="0" boxShadow="xl">
+          <VStack borderRadius="lg" spacing="0" boxShadow="xl" w="sm">
             <SongCard playingTrack={playingTrack} isPlaying={isPlaying} setIsPlaying={setIsPlaying} pauseOrResumeSong={pauseOrResumeSong} />
             <Box w="100%" h="2px" bg="gray.800" />
-            <StatusBar teamsToken={teamsToken} isPlaying={isPlaying} pauseOrResumeSong={pauseOrResumeSong} setPushToTeams={setPushToTeams} previousSong={previousSong} nextSong={nextSong} />
+            <StatusBar teamsToken={teamsToken} setTeamsToken={setTeamsToken} isPlaying={isPlaying} pauseOrResumeSong={pauseOrResumeSong} setPushToTeams={setPushToTeams} previousSong={previousSong} nextSong={nextSong} />
           </VStack>
           <Button onClick={() => { forcePushToTeams() }}>Force push to teams</Button>
-          <Button onClick={() => { outputSongToConsole() }}>Output to console</Button>
+          <Button onClick={() => {
+            outputSongToConsole();
+          }}>Output to console</Button>
         </VStack>
       )
     }
